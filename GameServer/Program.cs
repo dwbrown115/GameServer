f9a97a54d66586c8dc1a401da7c16f60953261bc
Dakota <dakota.w.brown@silvercastledigital.com>
2025-08-12 16:00:58 -0500

    Switched from http to https

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -62,7 +62,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) { }
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseWebSockets();
 app.UseAuthentication();
 
